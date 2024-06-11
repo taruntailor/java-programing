11fd64f83169b7682966b4bad22aa83eca075b72
Ronny_tailor <117915739+taruntailor@users.noreply.github.com>
2024-06-11 00:45:56 -0700

    Add files via upload
    java change

---
+++ module_2/q_003.java
@@ -0,0 +1,15 @@
+/*W.A.J.P to compare a given string to the specified character
+sequence. Comparing topsint.com and topsint.com: true
+Comparing Topsint.com and topsint.com: false*/
+package module_2;
+
+public class q_003 {
+	public static void main(String[] args) {
+		String str1 ="java.com";
+		String str2 ="java.com";
+		
+		boolean result =str1.equalsIgnoreCase(str2);
+		System.out.println("comparing"+str1+"and"+str2+":");
+		
+	}
+}
